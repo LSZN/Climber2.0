5cba548376d8e151557b15e0b8fe6fa18dcca2c1
171250014 <171250014@smail.nju.edu.cn  git config --global user.email 171250014@smail.nju.edu.cn  git config --global user.email 171250014@smail.nju.edu.cn>
2018-04-08 16:09:05 +0800

    try1

---
+++ EL2/app/src/main/java/com/example/lsn/end/EndingActivity.java
@@ -0,0 +1,27 @@
+package com.example.lsn.end;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lsn.end.border.Border;
+
+public class EndingActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ending);
+
+    }
+}
---
+++ EL2/app/src/test/java/stone_tablet/Stone_tablet.java
@@ -0,0 +1,8 @@
+package stone_tablet;
+
+/**
+ * Created by lsn on 2018/4/5.
+ */
+
+public class Stone_tablet {
+}
